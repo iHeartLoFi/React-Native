d40cc018f4582ecbd1a094b276a1f9f52bfb0e25
DM Chandler <73678692+iHeartLoFi@users.noreply.github.com>
2022-08-29 11:33:04 -0400

    Navigation Icons

---
+++ screens/AboutScreen.js
@@ -0,0 +1,47 @@
+import { PARTNERS } from '../shared/partners';
+import { useState } from "react";
+import { ScrollView } from "react-native-gesture-handler";
+import { Avatar,Card, ListItem } from "react-native-elements";
+import { Text } from "react-native";
+
+
+
+const AboutScreen = () => {
+    const [partners] =useState(PARTNERS);
+    const Mission = () => {
+        return(
+            <Card>
+                <Card.Title>Our Mission</Card.Title>
+                    <Card.Divider></Card.Divider>
+                    <Text style={{ margin: 10 }}>
+                        We present a curated database of the best campsites in the vast woods and back country of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                    </Text>
+            </Card>
+        )
+    };
+    return (
+        <ScrollView>
+          <Mission />
+          <Card>
+            <Card.Title>Community Partners</Card.Title>
+            <Card.Divider />
+            {partners.map((partner) => {
+              return (
+                <ListItem key={partner.id}>
+                  <Avatar source={partner.image} rounded />
+                  <ListItem.Content>
+                    <ListItem.Title>{partner.name}</ListItem.Title>
+                    <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                  </ListItem.Content>
+                </ListItem>
+              );
+            })}
+          </Card>
+        </ScrollView>
+      );
+
+
+};
+
+
+export default AboutScreen;
